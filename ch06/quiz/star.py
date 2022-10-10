45402a2c01650e2625bf09fe9a0eab2e94582094
HEEYA\HeeYa <rnldyatkd075@nate.com>
2022-10-10 20:57:24 +0900

    6장 (프로그램의 흐름 제어하기)

--- ch06/quiz/star.py
+++ ch06/quiz/star.py
@@ -5,11 +5,18 @@ for i in range(0, 5):
     for j in range(0, i+1):
         star = star + '*'
     print(star)
-    
-    
-    
+                            # *
+                            # **
+                            # ***
+                            # ****
+                            # *****
 for i in range(5, 0, -1):
     star = ''
     for j in range(0, i):
         star = star + '*'
     print(star)
+                            # *****
+                            # ****
+                            # ***
+                            # **
+                            # *
